2857235bb6049542b91b4f7021b5955e66af77c9
Robertstein <robertovenezia95@gmail.com>
2023-05-09 09:10:17 +0200

    Lobby / Main menu

--- MultiplayerTest/Source/MultiplayerTest/PuzzlePlatformGameInstance.cpp
+++ MultiplayerTest/Source/MultiplayerTest/PuzzlePlatformGameInstance.cpp
@@ -3,18 +3,43 @@
 
 #include "PuzzlePlatformGameInstance.h"
 #include "Engine/Engine.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Blueprint/UserWidget.h"
 
 
 UPuzzlePlatformGameInstance::UPuzzlePlatformGameInstance(const FObjectInitializer& ObjectInitializer)
 {
-	UE_LOG(LogTemp, Warning, TEXT("Costruttore"));
+	ConstructorHelpers::FClassFinder<UUserWidget> MenuBPClass(TEXT("/Game/MenuSystem/WBP_MainMenu"));
+
+	if (MenuBPClass.Class == nullptr) return;
+
+	MenuClass = MenuBPClass.Class;
 }
 
 
 void UPuzzlePlatformGameInstance::Init()
 {
-	UE_LOG(LogTemp, Warning, TEXT("Init"));
 
+	UE_LOG(LogTemp, Warning, TEXT("Found class %s"), *MenuClass->GetName());
+
+}
+
+void UPuzzlePlatformGameInstance::LoadMenu()
+{
+	if (MenuClass == nullptr) return;
+
+	UUserWidget* Menu = CreateWidget(this, MenuClass);
+	if (Menu == nullptr) return;
+	Menu->AddToViewport();
+
+	APlayerController* PlayerController = GetFirstLocalPlayerController();
+	if (PlayerController == nullptr) return;
+
+	FInputModeUIOnly InputModeData;
+	InputModeData.SetWidgetToFocus(Menu->TakeWidget());
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	PlayerController->SetInputMode(InputModeData);
+	PlayerController->bShowMouseCursor = true;
 }
 
 void UPuzzlePlatformGameInstance::Host()
@@ -23,6 +48,11 @@ void UPuzzlePlatformGameInstance::Host()
 	if (Engine == nullptr) return;
 
 	Engine->AddOnScreenDebugMessage(0, 2, FColor::Green, TEXT("Hosting"));
+
+	UWorld* World = GetWorld();
+	if (World == nullptr) return;
+
+	World->ServerTravel("/Game/ThirdPerson/Maps/ThirdPersonMap?listen");
 }
 
 void UPuzzlePlatformGameInstance::Join(const FString& Address)
@@ -31,4 +61,9 @@ void UPuzzlePlatformGameInstance::Join(const FString& Address)
 	if (Engine == nullptr) return;
 
 	Engine->AddOnScreenDebugMessage(0, 2, FColor::Green, FString::Printf(TEXT("Joining %s"), *Address));
+
+	APlayerController* PlayerController = GetFirstLocalPlayerController();
+	if (PlayerController == nullptr) return;
+
+	PlayerController->ClientTravel(Address, ETravelType::TRAVEL_Absolute);
 }
